6935d01aee98a0e541ce5c46fd742673c47abcc0
Ritu Gatiyala <ritu.gatiyala@toptal.com>
2024-01-20 23:19:09 +0530

    Adds players edit components

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,31 @@
+/**
+ * React imports
+ */
 
+import { useState } from "react";
+
+/**
+ * Component Imports
+ */
+
+import Player from "./components/Player";
+
+/**
+ * Main Component Export
+ */
 function App() {
-  
-
   return (
-    <h1>React Tic-Tac-Toe</h1>
-  )
+    <main>
+      <div id="game-container">
+        <ol id="players">
+          <Player initialName="Player1" symbol="X" />
+          <Player initialName="Player2" symbol="O" />
+        </ol>
+        GAME BOARD
+      </div>
+      LOG
+    </main>
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/Player.jsx
@@ -0,0 +1,47 @@
+/**
+ * React imports
+ */
+
+import { useState } from "react";
+
+/**
+ * Main Component Export
+ */
+export default function Player({ initialName, symbol }) {
+  const [playerName, setPlayerName] = useState(initialName);
+  const [isEditing, setIsEditing] = useState(false);
+
+  function handleEditClick() {
+    //   setIsEditing(!isEditing); // A shorter approach but is not recommended
+    setIsEditing((wasEditing) => !wasEditing);
+
+    // if (isEditing) setIsEditing(false);
+    // else setIsEditing(true);
+  }
+
+  function handleInputChange(event) {
+    setPlayerName(event.target.value);
+  }
+
+  let editablePlayerName = <span className="player-name">{playerName}</span>;
+  let btnCaption = "Edit";
+
+  if (isEditing) {
+    editablePlayerName = (
+      <input type="text" value={playerName} onChange={handleInputChange} />
+    );
+    btnCaption = "Save";
+  }
+
+  return (
+    <li>
+      <span className="player">
+        {editablePlayerName}
+
+        <span className="player-symbol">{symbol}</span>
+      </span>
+
+      <button onClick={handleEditClick}>{btnCaption} </button>
+    </li>
+  );
+}
